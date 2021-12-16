62883005270d34f12d29b08602f09eb9b045e829
MuhammedAlaa <soupermado@gmail.com>
2021-12-16 23:07:05 +0200

    fix: fix admin signup

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::AdminsController < ApplicationController
       @user_auth = UserAuth.create(email: permited_params[:email], password: permited_params[:password],
                               password_confirmation: permited_params[:password_confirmation], user_name: permited_params[:user_name], role: role)
       @admin = Admin.create(first_name: permited_params[:first_name], last_name: permited_params[:last_name],
-                                 birthday: permited_params[:birthday])
+                                 birthday: permited_params[:birthday], user_auth_id: @user_auth.id)
     end
     @data = { name: "#{@admin.first_name} #{@admin.last_name}", email: @user_auth.email,
                   user_name: @user_auth.user_name, birthday: @admin.birthday }
