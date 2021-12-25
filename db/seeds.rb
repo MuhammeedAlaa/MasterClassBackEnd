ae008ad010280e517672e94457636bb7c0da67f7
MuhammedAlaa <soupermado@gmail.com>
2021-12-25 15:06:22 +0200

    dev: adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_auth = UserAuth.create(email: 'muhammad.ahmed99@eng-st.cu.edu.eg', password: 'password', password_confirmation: 'password', user_name: 'Alaa', role: 'admin')
+Admin.create(first_name: 'Muhammad', last_name: 'Alaa', birthday: Time.now - 22.years, user_auth_id: user_auth.id)
+
+instructor = UserAuth.create(email: 'muhammad.hesham99@eng-st.cu.edu.eg', password: 'password', password_confirmation: 'password', user_name: 'Etsh', role: 'instructor')  
+Instructor.create(first_name: 'Muhammad', last_name: 'Ahmed Hisham', birthday: Time.now - 22.years, user_auth_id: instructor.id)
+
+user_auth = UserAuth.create(email: 'muhmoud.amr99@eng-st.cu.edu.eg', password: 'password', password_confirmation: 'password', user_name: 'Goudy', role: 'learner')
+Learner.create(first_name: 'Muhamoud', last_name: 'Amr', birthday: Time.now - 22.years, user_auth_id: user_auth.id)
+
+user_auth = UserAuth.create(email: 'houssam.alaa99@eng-st.cu.edu.eg', password: 'password', password_confirmation: 'password', user_name: 'Houso', role: 'learner')
+Learner.create(first_name: 'Housam', last_name: 'Alaa', birthday: Time.now - 22.years, user_auth_id: user_auth.id)
+
+Course.create!(name: 'Consultation', user_auth: instructor, about: 'this course to understand the basic concepts of conultation and security')
