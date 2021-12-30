db21a8665b9580a8358a2645b8809bcfc4d15afe
MuhammedAlaa <soupermado@gmail.com>
2021-12-30 21:14:11 +0200

    Fix: threads image

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,17 +2,17 @@
 
 # class for comments
 class Api::V1::CommentsController < ApplicationController
-  
   wrap_parameters false
   before_action :authenticate_any
 
-  def create 
+  def create
     permited_params = comment_params
     @course = Course.find(permited_params[:course_id])
     permited_params.delete('course_id')
     @comment = @course.comments.new(comment_params)
     @comment.user_auth = @user_auth
-    @data = {comment_id: @comment.id, course_id: @course.id, user_id: @user_auth.id, parent_id: @comment.parent_id, body: @comment.body}
+    @data = { comment_id: @comment.id, course_id: @course.id, user_id: @user_auth.id, parent_id: @comment.parent_id,
+              body: @comment.body }
     if @comment.save
       render status: :created
     else
@@ -28,7 +28,8 @@ class Api::V1::CommentsController < ApplicationController
     @threads.each do |thread|
       @replier = UserAuth.find(thread.user_auth_id)
       thread_image
-      @info = {id: thread.id, course_id: thread.course_id, body: thread.body, image: @image, user_name: @replier.user_name}
+      @info = { id: thread.id, course_id: thread.course_id, body: thread.body, image: @image,
+                user_name: @replier.user_name }
       @data.push(@info)
     end
     render status: :ok
@@ -42,31 +43,34 @@ class Api::V1::CommentsController < ApplicationController
     @threads_comment.each do |comment|
       @replier = UserAuth.find(comment.user_auth_id)
       thread_image
-      @info = {id: comment.id, course_id: comment.course_id, body: comment.body, image: @image, parent_id: comment.parent_id, user_name: @replier.user_name, parent_body: @thread.body}
+      @info = { id: comment.id, course_id: comment.course_id, body: comment.body, image: @image,
+                parent_id: comment.parent_id, user_name: @replier.user_name, parent_body: @thread.body }
       @data.push(@info)
     end
     render status: :ok
   end
 
-
   private
 
   def comment_params
-      params.require(:comment).permit(:body, :parent_id, :course_id)
+    params.require(:comment).permit(:body, :parent_id, :course_id)
   end
+
   def thread_params
-      params.permit(:course_id)
+    params.permit(:course_id)
   end
+
   def thread_comments_params
-      params.permit(:thread_id)
+    params.permit(:thread_id)
   end
+
   def thread_image
     @image = if @replier.role == 'admin'
-                          @replier.admin.image.url
-                        elsif @replier == 'instructor'
-                          @replier.instructor.image.url
-                        else
-                         @replier.learner.image.url
-                        end
+               @replier.admin.image.url
+             elsif @replier.role == 'instructor'
+               @replier.instructor.image.url
+             else
+               @replier.learner.image.url
+             end
   end
 end
